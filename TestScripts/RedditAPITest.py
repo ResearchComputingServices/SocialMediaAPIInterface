ebbd2b1591f104a0a0f772bf0742118f9b450067
Nicholi Shiell <nicholishiell@gmail.com>
2023-04-17 09:00:44 -0400

    completed reddit API test

--- TestScripts/RedditAPITest.py
+++ TestScripts/RedditAPITest.py
@@ -1,43 +1,170 @@
-CLIENT_ID = '_-W7ANd6UN4EXexvgHn8DA'
-SECRET_TOKEN = 'kpBdT1f-nRHM_kxdBzmxoOnDo_96FA'
-USERNAME = 'nickshiell'
-PASSWORD = 'Q!w2e3r4'
-
-# This Python script is used to test Reddit API commands
-
 import requests
+from Utils import *
 
-# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, 
-                                   SECRET_TOKEN)
+####################################################################################################
+# This function returns a header with authorization TOKEN 
+####################################################################################################
+def GetAuthentifiedHeader(  credDict,
+                            headerKey = 'User-Agent',
+                            headerValue = 'MyAPI/0.0.1'):
+    # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+    auth = requests.auth.HTTPBasicAuth( credDict['CLIENT_ID'],
+                                        credDict['SECRET_TOKEN'])
+       
+    # setup our header info, which gives reddit a brief description of our app
+    headers = {headerKey: headerValue}
 
-# here we pass our login method (password), username, and password
-data = {'grant_type': 'password',
-        'username': USERNAME,
-        'password': PASSWORD}
+    # send our request for an OAuth token
+    resp = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, 
+                        data=credDict, 
+                        headers=headers)
+    
+    # Check if there was an error message returned
+    Check4ResponseError(resp)
 
-# setup our header info, which gives reddit a brief description of our app
-headers = {'User-Agent': 'MyAPI/0.0.1'}
+    # convert response to JSON and pull access_token value
+    TOKEN = resp.json()['access_token']
 
-# send our request for an OAuth token
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, 
-                    data=data, 
-                    headers=headers)
+    # add authorization to our headers dictionary
+    headers['Authorization'] = f"bearer {TOKEN}"
+
+    return headers
+
+####################################################################################################
+# This function returns the top <N> posts in the subreddit <SR>
+####################################################################################################
+def GetTopPosts(   headers, 
+                    subRedditName, 
+                    numberOfPosts = 25):
+
+    urlString = API_BASE + 'r/'+subRedditName+'/top'
+
+    print('API Call: ', urlString)
+
+    resp = requests.get(    urlString,
+                            headers=headers,
+                            params={'limit':str(numberOfPosts)})
+    
+    return resp
+
+####################################################################################################
+# This function returns the comments for a given post
+#################################################################################################### 
+def GetCommentsFromPost(    headers, 
+                            subRedditName, 
+                            postID):
+
+    urlString = API_BASE + 'r/'+subRedditName+'/comments/'+postID
+
+    print('API Call: ', urlString)
+
+    resp = requests.get(    urlString,
+                            headers=headers)
+    
+    return resp
+
+####################################################################################################
+# This function returns all comments/replies from a given user
+####################################################################################################
+def GetUserComments(headers, userName):
+    urlString = API_BASE + 'user/'+userName+'/comments'
+    
+    print('API Call: ', urlString)
+
+    resp = requests.get(    urlString,
+                            headers=headers)
+    
+    return resp
+
+####################################################################################################
+# This function does a keyword search of a subreddit
+####################################################################################################
+def GetSubredditKeywordSearch(headers, subreddit, keyword):
+    urlString = API_BASE + 'r/'+subreddit + '/search/'
+    
+    print('API Call: ', urlString)
+
+    resp = requests.get(    urlString,
+                            headers=headers,
+                            params={'q':keyword, 'restrict_sr': True, 'limit' : 1000, })
+    
+    return resp
+
+####################################################################################################
+# This function returns replies to a comment 
+####################################################################################################
+def ExtractRepliesFromComment(comment):
+        
+    replies = {}
+    
+    if 'replies' in comment.keys():
+        replies = comment['replies']
+                
+        for reply in replies['data']['children']:
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ REPLY DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            DisplayDict(reply['data'], COMMENT_KEYS_OF_INTEREST)
+            input()
 
 
-# Check if there was an error message returned
-if 'error' in res.json().keys():  
-    print('Error Code:', res.json()['error'])
-    print('Error Msg:',res.json()['error_description'])
-    input('Press ENTER to continue...')
+    return replies
+ 
+####################################################################################################
+# Main Script
+# This Python script is used to test Reddit API commands
+####################################################################################################
 
+subreddit = 'python'
 
-# convert response to JSON and pull access_token value
-TOKEN = res.json()['access_token']
+credDict = GenerateCredentialsDict('nicksCreds.dat')
 
-# add authorization to our headers dictionary
-headers['Authorization'] = f"bearer {TOKEN}"
+header = GetAuthentifiedHeader(credDict)
 
-# while the token is valid (~2 hours) we just add headers=headers to our requests
-requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+# topPostsResponse = GetTopPosts(header, subreddit, 25)
+
+# Check4ResponseError(topPostsResponse)
+
+# for aPost in topPostsResponse.json()['data']['children']:
+   
+#     authorID = ExtractID36(aPost['data']['author_fullname'])
+#     postID = ExtractID36(aPost['data']['name'])    
+
+#     ups = aPost['data']['ups']
+#     downs = aPost['data']['downs']
+
+    # print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ POST DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    # print('authorID: ', authorID)
+    # print('postID: ', postID)
+    # print('up votes: ', ups)
+    # print('down votes: ', downs)
+    # DisplayDict(aPost['data'], POST_KEYS_OF_INTEREST)
+
+    # Look at the comments for each post    
+    # comments = GetCommentsFromPost(header, subreddit, postID)
+        
+    # for comment in comments.json():
+    #     for reply in comment['data']['children']:
+    #         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ COMMENT DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    #         DisplayDict(reply['data'],COMMENT_KEYS_OF_INTEREST)   
+    #         ExtractRepliesFromComment(reply['data'])
+    #         input()
+       
+    # Look at other posts and comments by the user of the current top post
+      
+    # userComments = GetUserComments(header, aPost['data']['author'])       
+    # Check4ResponseError(userComments)
+    # for comment in userComments.json()['data']['children']:
+    #     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ USER COMMENT DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    #     DisplayDict(comment['data'])
+    #     input()
+
+keywordSearch = GetSubredditKeywordSearch(header, 'python', 'learn')
+Check4ResponseError(keywordSearch)
+
+print(len( keywordSearch.json()['data']['children']))
+input()
+
+for result in keywordSearch.json()['data']['children']:
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ KEYWORD DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    DisplayDict(result['data'])
+    input()
--- TestScripts/Utils.py
+++ TestScripts/Utils.py
@@ -1,5 +1,5 @@
 import os
-
+from colorama import Fore, Back, Style
 
 ####################################################################################################
 # GLOBAL CONSTANTS
@@ -8,7 +8,104 @@ import os
 BASE_DIR = '/home/nickshiell/Documents/Work/SocialMediaAPIInterface/SocialMediaAPIInterface'
 CREDENTIALS_DIR = os.path.join(BASE_DIR, 'Creds')
 
+
+API_BASE = 'https://oauth.reddit.com/'
+
+POST_KEYS_OF_INTEREST = [   'subreddit',
+                            'selftext',
+                            'author_fullname',
+                            'title','downs',
+                            'name',
+                            'upvote_ratio',
+                            'ups',
+                            'score',
+                            'created',
+                            'view_count',
+                            'id',
+                            'author',
+                            'num_comments',
+                            'created_utc',
+                            'num_crossposts']
+
+COMMENT_KEYS_OF_INTEREST = [    'subreddit_id',
+                                'subreddit', 	
+                                'replies',
+                                'id',
+                                'author', 	
+                                'parent_id', 	
+                                'score', 	
+                                'author_fullname', 	
+                                'body', 	
+                                'name', 	
+                                'body_html', 	
+                                'created', 	
+                                'link_id', 	
+                                'controversiality', 	
+                                'depth', 	
+                                'ups'] 	
+
+
 ####################################################################################################
 # UTILITY FUNCTIONS
 ####################################################################################################
 
+####################################################################################################
+# This function reads a users credentials from a file and loads them into a dictionary
+# which can be passed to the Reddit server for authenification
+
+def GenerateCredentialsDict(filename):
+    
+    credDict = {'grant_type': 'password'}
+    
+    filePath = os.path.join(CREDENTIALS_DIR, filename)
+    
+    file = open(filePath, 'r')
+    
+    lines = file.readlines()
+            
+    for line in lines:
+        lineSplit = line.split(':')
+        
+        if len(lineSplit) == 2:
+            credDict[lineSplit[0].strip()] = lineSplit[1].strip()
+        
+    return credDict
+
+####################################################################################################
+# This function checks a response for an error. If there is an error than the error code and any
+# error text is displayed. If HALT is True than the script waits for the user to hit enter
+def Check4ResponseError(resp, HALT = True):
+    if 'error' in resp.json().keys():  
+        print('Error Code:', resp.json()['error'])
+
+        if 'error_description' in resp.json().keys():
+            print('Error Msg:',resp.json()['error_description'])
+        
+        if HALT:    
+            input('Press ENTER to continue...')
+
+####################################################################################################
+# This function removed the 'type' code at the front of a reddit ID36
+def ExtractID36(str):
+    
+    id36 = None
+    
+    strSplit = str.split('_')
+    
+    if len(strSplit) == 2:
+        id36 = strSplit[1]
+    
+    return id36
+
+####################################################################################################
+# This function prints the contents of a dictionary outwith colours
+def DisplayDict(dict, keysOfInterest = None):
+    
+    for key in dict.keys():
+        
+        if keysOfInterest != None and key not in keysOfInterest:
+            continue        
+            
+        value = dict[key]
+       
+        print(Fore.RED + key, Fore.WHITE+':',value)
