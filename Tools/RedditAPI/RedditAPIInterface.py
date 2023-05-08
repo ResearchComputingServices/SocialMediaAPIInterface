dbeacf8d50bb8bc703ede83b0d891dc635b81496
Nicholi Shiell <nicholishiell@gmail.com>
2023-05-08 09:10:35 -0400

    added cmd line interface

--- Tools/RedditAPI/RedditAPIInterface.py
+++ Tools/RedditAPI/RedditAPIInterface.py
@@ -128,17 +128,20 @@ def GetUserComments(headers,
 ####################################################################################################
 def GetUserPosts(   headers, 
                     userName, 
-                    params = {}, 
+                    sortType,
+                    timeFrame,
                     numResponses = MAX_NUM_RESPONSES_TOTAL):
 
     urlString = API_BASE + 'user/' + userName + '/submitted' 
+    
+    params = {}
+    params['sort'] = sortType
+    params['t'] = timeFrame
     
     return RequestGet(urlString=urlString,
                       header=headers,
                       params=params,
                       numResultsRequested=numResponses)
-
-
 
 ####################################################################################################
 # This function performs a GET call from requests as defined by it's arguments
@@ -180,28 +183,90 @@ def RequestGet(urlString, header, params, numResultsRequested = 100):
 # This block of code is the command line interface for working with the functions in this library
 ####################################################################################################
 
-def HandleCommandLineArgs(cmdLineArgs):
+def ExtractCommandLineArgs(cmdLineArgs):
     
     optionsDict = {}
     
     numCmdLineArgs = len(cmdLineArgs)
     
-    for i in range(0,numCmdLineArgs):
+    for i in range(1,numCmdLineArgs):
+
         option = cmdLineArgs[i]
+
+        if option[0] != '-':
+            continue
         
-        if option == '--subreddit':
+        if option == '-cred':
+            i = i + 1
+            optionsDict['credFilename'] = cmdLineArgs[i]
+            
+        elif option == '-subreddit':
             i = i + 1
             optionsDict['subReddit'] = cmdLineArgs[i]
         
-        elif option == '--user':
+        elif option == '-user':
             i = i + 1
-            optionsDict['user'] = cmdLineArgs[i]    
+            optionsDict['user'] = cmdLineArgs[i] 
+        
+        elif option == '-posts':
+            i = i + 1
+            optionsDict['getPosts'] = '' 
+        
+        elif option == '-commments':
+            i = i + 1
+            optionsDict['getComments'] = '' 
+           
+        elif option == '-t':
+            i = i + 1
+            optionsDict['timeFrame'] = cmdLineArgs[i] 
+        
+        elif option == '-s':
+            i = i + 1
+            optionsDict['sortBy'] = cmdLineArgs[i] 
+        
+        elif option == '-l':
+            i = i + 1
+            optionsDict['limit'] = cmdLineArgs[i] 
+        
+        elif option == '--keyword':
+            i = i + 1
+            optionsDict['keyword'] = cmdLineArgs[i] 
+        
+        else:
+            print('[WARNING]: HandleCommandLineArgs: Unknown command line option:',option)        
         
     return optionsDict   
 
+####################################################################################################
+def HandleOptionsdict(optionsDict):
+    
+    credDict = GenerateCredentialsDict(optionsDict['credFilename'])
+    header = GetAuthentifiedHeader(credDict)
+
+    subRedditName =''
+    sortBy = ''
+    timeFrame = ''
+    numResponses = MAX_NUM_RESPONSES_TOTAL
+    
+    if 'subreddit' in optionsDict.keys():
+        subRedditName=optionsDict['subreddit']
+
+
+    if 'getPosts' in optionsDict.keys():
+        postsResponseList = GetSubredditPosts(  headers=header,
+                                                subRedditName=subRedditName,
+                                                category=optionsDict['sortBy'],
+                                                timeFrame=optionsDict['timeFrame'],
+                                                numResponses=optionsDict['limits'])
+
+
+
+####################################################################################################
+
 if __name__ == '__main__':
-    print('COMMAD LINE INTERFACE')
-    optionsDict = HandleCommandLineArgs(sys.argv)
+    optionsDict = ExtractCommandLineArgs(sys.argv)    
     
     for option in optionsDict.keys():
         print(option,':',optionsDict[option])
+        
+    HandleOptionsdict(optionsDict)
